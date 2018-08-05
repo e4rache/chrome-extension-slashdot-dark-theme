4a048fc0b02b3f292bda9c32a7eb33fe079220dd
e4rache <e4rache@protonmail.com>
2018-08-05 14:34:35 +0200

    h4 comments

--- content.js
+++ content.js
@@ -5,31 +5,43 @@ const red = "rgb(200,100,100)";
 
 let d = document;
 
-let divs = d.getElementsByTagName("div");
+let divs = d.querySelectorAll("div");
 for (e of divs) {
   e.style["background-color"] = dark;
   e.style["color"] = light;
 }
 
-let sections = d.getElementsByTagName("section");
+let sections = d.querySelectorAll("section");
 for (e of sections) {
   e.style["background-color"] = dark;
   e.style["color"] = light;
 }
 
-let body = d.getElementsByTagName("body")[0];
+let body = d.querySelectorAll("body")[0];
 body.style["background-color"] = dark;
 body.style["color"] = light;
 
-let h2 = d.getElementsByTagName("h2");
+let h2 = d.querySelectorAll("h2");
 for (e of h2) {
   e.style.backgroundColor = black;
 }
 
-let links = d.getElementsByTagName("a");
+let h4 = d.querySelectorAll("h4");
+for (e of h4) {
+  e.style.background = black;
+  e.style["background-image"] =
+    "-webkit-gradient(linear, 0% 0%, 0% 100%, from(rgb(0,0,0)), to(rgb(0,0,0)));";
+}
+
+let titles = d.querySelectorAll(".title");
+for (t of titles) {
+  t.style.backgroundColor = black;
+}
+
+let links = d.querySelectorAll("a");
 for (e of links) {
   e.style.color = red;
 }
 
-let announcement = d.getElementById("announcement");
+let announcement = d.querySelector("#announcement");
 announcement.parentNode.removeChild(announcement);
